14d5bcae82bb62875d6027f9ce0d46671e7cc66a
oise akhibi <oiseu2tall@googlemail.com>
2026-06-17 11:14:42 +0100

    fixed db url issues

--- prisma/migrations/20260606113144_init3/migration.sql
+++ prisma/migrations/20260606113144_init3/migration.sql
@@ -1,11 +1,26 @@
--- DropForeignKey
-ALTER TABLE "cohorts" DROP CONSTRAINT "cohorts_managerId_fkey";
+-- Conditionally drop foreign keys (ignore if they don't exist)
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.table_constraints
+    WHERE constraint_name = 'cohorts_managerId_fkey'
+  ) THEN
+    ALTER TABLE "cohorts" DROP CONSTRAINT "cohorts_managerId_fkey";
+  END IF;
 
--- DropForeignKey
-ALTER TABLE "courses" DROP CONSTRAINT "courses_managerId_fkey";
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.table_constraints
+    WHERE constraint_name = 'courses_managerId_fkey'
+  ) THEN
+    ALTER TABLE "courses" DROP CONSTRAINT "courses_managerId_fkey";
+  END IF;
+END $$;
 
 -- AddForeignKey
 ALTER TABLE "cohorts" ADD CONSTRAINT "cohorts_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "courses" ADD CONSTRAINT "courses_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
